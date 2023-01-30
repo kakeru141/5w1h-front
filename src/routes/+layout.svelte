2bcd9fb98232afb59429dcb3978e802d975935ce
kakeru141 <senruttyo@gmail.com>
2023-01-30 20:14:07 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<slot />
+
+<style>
+	:global(a) {
+		color: inherit;
+		text-decoration: none;
+	}
+	:global(button) {
+		cursor: pointer;
+	}
+	:global(body) {
+		font-family: LINESeedJP;
+		src: url(static/fonts/LINESeedJP_OTF_Bd.woff);
+		src: url(static/fonts/LINESeedJP_OTF_Eb.woff);
+		src: url(static/fonts/LINESeedJP_OTF_Rg.woff);
+		src: url(static/fonts/LINESeedJP_OTF_Th.woff);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="parent">
+    <div class="child"></div>
+</div>
+
+<style lang="scss">
+
+</style>
